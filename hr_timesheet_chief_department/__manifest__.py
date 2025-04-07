d9550596c8f5505dd80a5a2cf744fc931542a886
mikedat <miqueldat@gmail.com>
2025-04-07 12:10:53 +0000

    hr_timesheet_head modificado algo falla

--- hr_timesheet_chief_department/__manifest__.py
+++ hr_timesheet_chief_department/__manifest__.py
@@ -16,17 +16,20 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Services/Timesheets',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr_timesheet'],
+    'depends': ['base', 'hr_timesheet', 'analytic'],
 
     # always loaded
     "data": [
+        "security/ir_rules.xml",
         "security/ir.model.access.csv",
-        "views/hr_timesheets_chief_department_views_views.xml"
+        "views/hr_timesheets_chief_department_menu_menu.xml",
+        "views/hr_timesheets_chief_department_views_views.xml",
     ],
+     'application': True,
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
